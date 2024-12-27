53e3a73ee9112fe553594bd482919a9b2093715a
LuXuz <LuXuzStudios@gmail.com>
2024-12-27 12:00:34 -0500

    refactor: El Login retorna un json

--- SistemaAlquiler/SistemaAlquiler.API/obj/Debug/net8.0/SistemaAlquiler.API.AssemblyInfo.cs
+++ SistemaAlquiler/SistemaAlquiler.API/obj/Debug/net8.0/SistemaAlquiler.API.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAlquiler.Controladora")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAlquiler.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb44644102fa6529ebd2e48f7dc54a583ffdf1c7")]
-[assembly: System.Reflection.AssemblyProductAttribute("SistemaAlquiler.Controladora")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SistemaAlquiler.Controladora")]
+[assembly: System.Reflection.AssemblyProductAttribute("SistemaAlquiler.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SistemaAlquiler.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
