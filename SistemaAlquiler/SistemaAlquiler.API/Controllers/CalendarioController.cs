ba8606e2e184977a474702b4fb0185db8dcc50d4
Angel Hernandez <LuXuzStudios@gmail.com>
2025-01-13 20:20:07 -0500

    feat: Endpoint fechas no disponibles

---
+++ SistemaAlquiler/SistemaAlquiler.API/Controllers/CalendarioController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SistemaAlquiler.LogicaNegocio.DTOs;
+using SistemaAlquiler.Entidades;
+using SistemaAlquiler.LogicaNegocio.Interfaces;
+using System.Web.Http.Results;
+using SistemaAlquiler.LogicaNegocio.Implementaciones;
+
+namespace SistemaAlquiler.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CalendarioController:ControllerBase
+{
+    private readonly ICalendarioServicio calendarioServicio;
+
+    public CalendarioController(ICalendarioServicio calendarioServicio)
+    {
+        this.calendarioServicio = calendarioServicio;
+    }
+
+    [HttpGet("listaFechas")]
+    public async Task<IActionResult> listaFechas(int idCasa)
+    {
+        var fechas = await calendarioServicio.fechasOcupadas(idCasa);
+        return StatusCode(StatusCodes.Status200OK, fechas);
+    }
+}
